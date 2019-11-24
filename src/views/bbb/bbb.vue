db7b1a0c8e70e2daf2140bff0d80960a4cbc9b21
944245073@qq.com <944245073@qq.com>
2019-11-24 22:17:02 +0800

    递归2

---
+++ src/views/bbb/bbb.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+   
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import DetailList from '@/views/bbb/myTree.vue'
+  export default{
+    name: 'Detail',
+    components: {
+      DetailList
+    },
+    data(){
+      return {
+        list: [{
+          title: '成人票',
+          children: [{
+            title: '成人3馆联票',
+            children: [
+				{title: '成人1馆联票-成都'}, 
+				{title: '成人2馆联票-上海'}, 
+				{title: '成人3馆联票-北京'}, 
+				{title: '成人4馆联票-河南'},]
+          }, 
+				  {title: '成人4馆联票',
+		children:[{title:"123123123123123123"}]
+				  }, {title: '成人5馆联票'}, { title: '成人6馆联票'}]
+        },
+				{ title: '学生票'}, 
+				{ title: '儿童票'}, 
+				{title: '特惠票'}]
+      }
+    }
+  }
+</script>
---
+++ src/views/bbb/myTree.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="item" v-for="(item,index) of list" :key="index">
+ 
+      <div class="margin">
+        <span class="item-title-icon" > {{item.title}}</span>
+		<el-button size="small" type="primary" @click="del(index)">del</el-button>
+      </div>
+      <!--递归组件：my-tree调用自身-->
+      <div v-if="item.children" class="item-children">
+        <my-tree :list="item.children" ></my-tree>
+      </div>
+ 
+    </div>
+  </div>
+</template>
+<script>
+	export default {
+		name:"my-tree",
+		 props: {
+		    list: {
+		        type: Array,
+		        required: true
+		    }
+		},
+		data(){
+			return {
+				
+			}
+		},
+		methods:{
+			del(index){
+				console.log(index)
+				    this.list.splice(index, 1)
+		
+			}
+		}
+	}
+</script>
+
+<style  scoped>
+	.margin{
+			padding: 5px;
+	}
+</style>
